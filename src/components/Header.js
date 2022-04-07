8256edcc352e8d031e6ad8f437fecefa31ba00ae
Socerer <coolfool@socerer>
2022-04-07 14:45:41 +0530

    Creating Header

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='header'>
+        <div className='header__left'>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
